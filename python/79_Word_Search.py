6cbd0132b2675960e9c1d82f23bddfe9742ba3b2
hufeiya <a4188@qq.com>
2015-11-12 23:54:14 +0800

    卧槽大型实验都没人跟我组队只能自己干了

--- python/79_Word_Search.py
+++ python/79_Word_Search.py
@@ -11,7 +11,7 @@ class Solution(object):
         self.width = len(board[0])
         self.height = len(board)
         self.board = board;
-        self.zeroMatrix = [[0]*self.width for i in xrange(self.height)]
+        self.zeroMatrix = [[False]*self.width for i in xrange(self.height)]
         for i in xrange(self.height):
             for j in xrange(self.width):
                 self.dfs(i,j,0)
@@ -23,9 +23,9 @@ class Solution(object):
         if index == self.length - 1:
             self.result = True
             return
-        self.zeroMatrix[i][j] = 1
-        if j != 0 and self.zeroMatrix[i][j-1] == 0:self.dfs(i,j-1,index+1)
-        if j+1 != self.width and self.zeroMatrix[i][j+1] == 0:self.dfs(i,j+1,index+1)
-        if i != 0 and self.zeroMatrix[i-1][j] == 0:self.dfs(i-1,j,index+1)
-        if i+1 != self.height and self.zeroMatrix[i+1][j] == 0: self.dfs(i+1,j,index+1)
-        self.zeroMatrix[i][j] = 0
+        self.zeroMatrix[i][j] = True
+        if j != 0 and self.zeroMatrix[i][j-1] == False:self.dfs(i,j-1,index+1)
+        if j+1 != self.width and self.zeroMatrix[i][j+1] == False:self.dfs(i,j+1,index+1)
+        if i != 0 and self.zeroMatrix[i-1][j] == False:self.dfs(i-1,j,index+1)
+        if i+1 != self.height and self.zeroMatrix[i+1][j] == False: self.dfs(i+1,j,index+1)
+        self.zeroMatrix[i][j] = False
